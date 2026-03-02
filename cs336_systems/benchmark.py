eff91a04249464331983485fddae5c6f9c10af59
ThomasMousseau <thom.mousseau@gmail.com>
2026-03-02 16:45:04 -0500

    wip: will have to skip nsys_profile for now, on to precision and memory next

--- cs336_systems/benchmark.py
+++ cs336_systems/benchmark.py
@@ -1,13 +1,20 @@
+import math
+
+import cs336_basics
 from cs336_basics.model import BasicsTransformerLM
 import torch
 import torch.nn as nn
+import torch.cuda.nvtx as nvtx
+
+from einops import rearrange, einsum
+from cs336_basics.nn_utils import softmax
+from torch import Tensor
+from jaxtyping import Float, Bool, Int
+
 
 if not torch.cuda.is_available():
     print("CUDA is required to run this benchmark!")
 
-# def sync():
-#     if torch.cuda.is_available():
-#         torch.cuda.synchronize()
 
 def cuda_timer():
     start = torch.cuda.Event(enable_timing=True)
@@ -44,7 +51,7 @@ def build_model_and_batch(device: torch.device):
 
     return model, dummy_input_ids, dummy_targets
 
-def benchmark_forward(model, input_ids, warmup=3, iters=10):
+def benchmark_forward(model, input_ids, warmup=5, iters=10):
     model.eval() #! just in case there is dropout or something
     times = [] #!(time (ms), is_before_warmup) pairs
     with torch.no_grad():
@@ -59,12 +66,14 @@ def benchmark_forward(model, input_ids, warmup=3, iters=10):
     avg_excl_warmup = sum(t for t, is_after_warmup in times if is_after_warmup) / iters
     assert len(times) == warmup + iters
     avg_incl_warmup = sum(t for t, _ in times) / len(times)
-    print(f"[Forward]   avg over {iters} iters: {avg_excl_warmup:.2f} ms")
     print(f"[Forward]   avg over {warmup+iters} iters (incl. warmup): {avg_incl_warmup:.2f} ms")
+    print(f"[Forward]   avg over {iters} iters: {avg_excl_warmup:.2f} ms")
+    print(f"[Forward]   std dev over {iters} iters: {torch.std(torch.tensor([t for t, is_after_warmup in times if is_after_warmup])):.2f} ms")
+    
     return avg_excl_warmup
 
 
-def benchmark_backward(model, input_ids, targets, warmup=3, iters=10):
+def benchmark_backward(model, input_ids, targets, warmup=5, iters=10):
     model.train()
     loss_fn = nn.CrossEntropyLoss()
     optimizer = torch.optim.AdamW(model.parameters(), lr=1e-4)
@@ -86,10 +95,11 @@ def benchmark_backward(model, input_ids, targets, warmup=3, iters=10):
     avg_incl_warmup = sum(t for t, _ in times) / len(times)
     print(f"[Backward]  avg over {iters} iters: {avg_excl_warmup:.2f} ms")
     print(f"[Backward]  avg over {warmup+iters} iters (incl. warmup): {avg_incl_warmup:.2f} ms")
+    print(f"[Backward]  std dev over {iters} iters: {torch.std(torch.tensor([t for t, is_after_warmup in times if is_after_warmup])):.2f} ms")
     return avg_excl_warmup
 
 
-def benchmark_e2e(model, input_ids, targets, warmup=3, iters=10):
+def benchmark_e2e(model, input_ids, targets, warmup=5, iters=10):
     """Forward + backward + optimizer step."""
     model.train()
     loss_fn  = nn.CrossEntropyLoss()
@@ -110,13 +120,56 @@ def benchmark_e2e(model, input_ids, targets, warmup=3, iters=10):
 
     avg_excl_warmup = sum(t for t, is_after_warmup in times if is_after_warmup) / iters
     avg_incl_warmup = sum(t for t, _ in times) / len(times)
-    print(f"[E2E]       avg over {iters} iters: {avg_excl_warmup:.2f} ms  (last loss: {loss.item():.4f})")
+    print(f"[E2E]       avg over {iters} iters: {avg_excl_warmup:.2f} ms")
     print(f"[E2E]       avg over {warmup+iters} iters (incl. warmup): {avg_incl_warmup:.2f} ms")
+    print(f"[E2E]       std dev over {iters} iters: {torch.std(torch.tensor([t for t, is_after_warmup in times if is_after_warmup])):.2f} ms")
     return avg_excl_warmup
+
+def annotated_scaled_dot_product_attention(
+    Q: Float[Tensor, " ... queries d_k"],
+    K: Float[Tensor, " ... keys    d_k"],
+    V: Float[Tensor, " ... keys    d_v"],
+    mask: Bool[Tensor, " ... queries keys"] | None = None,
+) -> Float[Tensor, " ... queries d_v"]:
+    """Scaled dot-product attention.
+
+    This function implements Eq. 1 of the Transformer paper.
+
+    Args:
+        Q: Tensor of queries, may have any number of leading dimensions.
+        K: Tensor of keys, sharing leading dimensions with Q.
+        V: Tensor of values, sharding leading dimensions with Q and K.
+        mask: An (optional) mask of shape (..., seq_len, seq_len).
+            Attention scores for positions with a mask value of `False` should
+            be masked out, i.e., not affect the softmaxed attention probabilities.
+
+    Returns:
+        torch.FloatTensor of shape (..., seq_len, value_dimension)
+        with the output of running your scaled dot product attention
+        implementation with the provided key, query, and value tensors.
+    """
+
+    d_k = K.shape[-1]
+    with nvtx.range("Computing scaled dot product attention"):
+        #TODO: compare eisum vs transpose + matmul for computing attention scores (Thomas)
+        attention_scores = einsum(Q, K, "... query d_k, ... key d_k -> ... query key") / math.sqrt(d_k)
+
+    if mask is not None:
+        attention_scores = torch.where(mask, attention_scores, float("-inf"))
+
+    with nvtx.range("Computing softmax"):
+        attention_weights = softmax(attention_scores, dim=-1)  # Softmax over the key dimension
+
+    with nvtx.range("Computing matmul with values (V matrix)"):
+        result = einsum(attention_weights, V, "... query key, ... key d_v ->  ... query d_v")
+        
+    return result
 
 if __name__ == "__main__":
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     print(f"Device: {device}\n")
+    
+    cs336_basics.model.scaled_dot_product_attention = annotated_scaled_dot_product_attention
 
     model, input_ids, targets = build_model_and_batch(device)
 
